27667a096e49c48a5ad75f4d61cfbe5fe0f44305
0z17 <915208316@qq.com>
2024-12-27 09:58:50 +0800

    add a test demo

---
+++ build/CMakeFiles/collision_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/collision_test.dir/src/collision_checker_test.cpp.o"
+  "collision_test"
+  "collision_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/collision_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
